964e049b30c29ebe5dc56bec3ecaa6ea7c71c837
Crawdon <67289352+Crawdon@users.noreply.github.com>
2021-11-01 08:46:11 -0300

    Feedback visual agregado
    Added visual feedback to input fields

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-document.addEventListener("DOMContentLoaded",()=>{
+function setFormMessage(formElement,type,message){
+    const messageElement = formElement.querySelector(".form-message");
 
-    
+    messageElement.textContent = message;
+    messageElement.classList.remove("form-message-success","form-message-error");
+    messageElement.classList.add(`form-message-${type}`);
+}
+
+function setInputError(inputElement,message){
+    inputElement.classList.add("form-input-error")
+    inputElement.parentElement.querySelector(".form-input-error-message").textContent = message
+}
+function clearInput(inputElement){
+    inputElement.classList.remove("form-input-error")
+    inputElement.parentElement.querySelector(".form-input-error-message").textContent = ""
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+   
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
 
@@ -19,5 +35,36 @@ document.addEventListener("DOMContentLoaded",()=>{
         loginForm.classList.remove("form-hiden");
 
     })
+
+    loginForm.addEventListener("submit",e =>{
+        e.preventDefault();
+        //Ajax or fetch login
+        //setFormMessage(loginForm,"success","Sesión iniciada!")
+
+        setFormMessage(loginForm,"error","Usuario o contraseña invalida")
+    })
+
+    document.querySelectorAll(".form-input").forEach(inputElement =>{
+        inputElement.addEventListener("blur", e =>{
+            if (e.target.id === "singUpUsername" && e.target.value.length >0 && e.target.value.length< 10 ){
+                setInputError(inputElement, "El nombre de usuario debe tener al menos 10 caracteres")
+            }
+            if (e.target.id === "singUpEmail"  && e.target.value.length >0){
+                let mail = e.target.value
+                const word = '@';
+                if (!mail.includes(word)){
+                    setInputError(inputElement, "El mail ingresado es invailido")
+                }
+                
+            } 
+            
+            inputElement.addEventListener("input",e=>{
+                clearInput(inputElement);
+
+            })
+            
+        })
+    })
+
 })
 
